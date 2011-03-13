bba557bfea3e7d5d54911f2bd445f336ff46f194
Bradley Priest <bradleypriest@gmail.com>
2011-03-13 18:22:33 +1300

    Don't set PXPAY_CONFIG if the yml file doesn't exist yet

--- lib/pxpay/init.rb
+++ lib/pxpay/init.rb
@@ -1,1 +1,5 @@
-PXPAY_CONFIG = YAML.load_file("#{Rails.root}/config/pxpay.yml")[Rails.env]
+if File.exists?("#{Rails.root}/config/pxpay.yml")
+  PXPAY_CONFIG = YAML.load_file("#{Rails.root}/config/pxpay.yml")[Rails.env]
+else
+  print "Please run rails generate pxpay:install to generate a pxpay config file"
+end

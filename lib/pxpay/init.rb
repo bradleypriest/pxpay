0bdd8294328323f0db21b0a3a7a92013a16ef676
Bradley Priest <bradleypriest@gmail.com>
2011-03-14 23:56:37 +1300

    My first try at testing for yml did not work well

--- lib/pxpay/init.rb
+++ lib/pxpay/init.rb
@@ -1,5 +1,5 @@
 if File.exists?("#{Rails.root}/config/pxpay.yml")
   PXPAY_CONFIG = YAML.load_file("#{Rails.root}/config/pxpay.yml")[Rails.env]
 else
-  print "Please run rails generate pxpay:install to generate a pxpay config file"
+  PXPAY_CONFIG = []
 end

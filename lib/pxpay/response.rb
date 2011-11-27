c84f0eaf723251c1ff17ec3d14e55082db8d1ce9
Bradley Priest <bradleypriest@gmail.com>
2011-11-28 09:48:03 +1300

    Fix up the last release, was using a 0.2 feature in the 0.1 branch

--- lib/pxpay/response.rb
+++ lib/pxpay/response.rb
@@ -2,28 +2,28 @@ module Pxpay
   # The response object received from Payment Express
   class Response
     attr_accessor :post
-    
+
     # Create a new Payment Express response object by passing in the return parameters provided to the success/failure URL
-    
+
     def initialize(params)
       @result = params[:result]
       @user_id = params[:userid]
       @post = build_xml( params[:result] )
     end
-    
+
     # Retrieving the transaction details from Payment Express as an instance of Pxpay::Notification
     def response
       require 'rest_client'
-      response = ::RestClient.post( Pxpay::Base.pxpay_request_url,  self.post )
+      response = ::RestClient.post( "https://sec.paymentexpress.com/pxpay/pxaccess.aspx",  self.post )
       return ::Pxpay::Notification.new( response )
     end
-  
+
     private
     # Internal method to build the xml to send to Payment Express
     def build_xml( result )
       xml = ::Builder::XmlMarkup.new
-    
-      xml.ProcessResponse do 
+
+      xml.ProcessResponse do
         xml.PxPayUserId PXPAY_CONFIG[:pxpay][:pxpay_user_id]
         xml.PxPayKey PXPAY_CONFIG[:pxpay][:pxpay_key]
         xml.Response result
